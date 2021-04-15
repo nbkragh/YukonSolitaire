c51751ea559ed75d4212adeb44f951c1f46b8303
NBK <msungo85@gmail.com>
2021-04-15 15:52:49 +0200

    struct Card

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "CardFunctions.h"
 
 #define MAXCHAR 1000
+
+struct Card {
+    int  value;
+    char name[2];
+    int shown;
+};
 struct Node {
-    int data;
+    struct Card data;
     struct Node* next;
 };
 
 int main(){
     struct Node* C[7];
-
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
-  
+    struct Card* newCard = malloc(sizeof(struct Card));
+    
     // allocate 3 nodes in the heap
     for(int i = 0; i < 7; i++){
         C[i] = malloc(sizeof(struct Node));
         C[i]->next = (struct Node*)malloc(sizeof(struct Node));
-        C[i]->data = 3;
-        LoadCards();
+        newCard->value = i;
+        strcpy(newCard->name , "AA");
+        newCard->shown = 0;
+        C[i]->data = *newCard;
     }
     for(int i = 0; i < 7; i++){
-        printf(" %d",C[i]->data);
+        printf(" %d %s %d", (C[i]->data).value, (C[i]->data).name, (C[i]->data).shown);
     }
     FILE* inStream = fopen("Cards.txt","r"); 
     char str[200];
