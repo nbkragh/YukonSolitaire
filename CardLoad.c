2bfff4c58549e022a473a517bc781150b345cb57
Stig <s186333@student.dtu.dk>
2021-04-15 15:32:17 +0200

    Made a linklist that prints a string

--- CardLoad.c
+++ CardLoad.c
@@ -3,12 +3,57 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "CardFunctions.h"
 
 #define MAXCHAR 1000
 
+struct element {
+    char *data;
+    int key;
+    struct element* next;
+};
+
+struct element *head = NULL;
+struct element *current = NULL;
+
+void push(char *data, struct element** stack){
+    struct element* Element = (struct element*) malloc(sizeof (struct element));
+    Element ->data = data;
+    Element ->next = *stack;
+    (*stack) = Element;
+}
+
+void pop(struct element** stack){
+
+    if (stack != NULL){
+        printf("Element popped: %s\n", (*stack) -> data);
+        struct element* tempPtr = *stack;
+        *stack = (*stack) -> next;
+        free(tempPtr);
+    } else {
+        printf("The Stack is empty.\n");
+    }
+}
+
+void top(struct element* stack){
+    if (stack != NULL){
+        printf("Element on top: %s\n", stack -> data);
+    } else {
+        printf("The Stack is empty.\n");
+    }
+}
+
 void LoadCards(void){
-    printf("Hej!\n");
+    struct element* root = (struct element*) malloc(sizeof (struct element));
+    root ->data = "";
+    root ->next = NULL;
+
+
+    for (int i = 0; i < 10; ++i) {
+        push("[]",&root);
+        top(root);
+    }
 }
 
 void cardFromFile(void){
--- Solitare.c
+++ Solitare.c
@@ -11,17 +11,17 @@ int main(void){
     LoadCards();
     cardFromFile();
 
-    for (int i = 1; i <= 7; ++i ){
-        printf("C%d \t",i);
-    }
-    printf("\n");
-
-    for (int i = 0; i <= 7; ++i) {
-        for (int j = 1; j <= 7; ++j) {
-            printf("KC\t");
-        }
-        printf("\n");
-    }
+//    for (int i = 1; i <= 7; ++i ){
+//        printf("C%d \t",i);
+//    }
+//    printf("\n");
+//
+//    for (int i = 0; i <= 7; ++i) {
+//        for (int j = 1; j <= 7; ++j) {
+//            printf("KC\t");
+//        }
+//        printf("\n");
+//    }
     return 0;
 }
 
