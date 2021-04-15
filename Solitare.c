94c7d27714725005f11f83871e3cd9af5d94b301
Emil Trangeled <emilbfh@hotmail.com>
2021-04-15 13:24:57 +0200

    Made a list of cards in a txt file, and printing the columns

---
+++ Solitare.c
@@ -0,0 +1,24 @@
+//
+// Created by Emil Trangeled on 15-04-2021.
+//
+
+#include <stdio.h>
+
+int main(void){
+
+    for (int i = 1; i <= 7; ++i ){
+        printf("C%d \t",i);
+    }
+    printf("\n");
+
+    for (int i = 0; i <= 7; ++i) {
+        for (int j = 1; j <= 7; ++j) {
+            printf("KC\t");
+        }
+        printf("\n");
+    }
+
+}
+
+
+
