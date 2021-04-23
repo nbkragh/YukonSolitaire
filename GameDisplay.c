5b493f232464e62f1aad96116868fdd4da2c42ae
Emil Trangeled <emilbfh@hotmail.com>
2021-04-23 09:43:07 +0200

    Command SW now has error-handling for a parameter, since it does not accept parameters.

--- GameDisplay.c
+++ GameDisplay.c
@@ -4,16 +4,20 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "CardFunctions.h"
 
 int displayGame(void) {
 
-    char input[25] ="",commands[25]="",parameter[25]="", lastCommand[25] = "", returnMsg[25] = "OK";
-    int i = 0;
+    char input[25] ="",commands[25]="",parameter[25]="", lastCommand[25] = "", returnMsg[50] = "OK";
+    int i;
     char *command[2];
+
 
     // while loop for the command loop
     while (strcmp(input, "QQ") != 0) {
+        // iterator for command pointer array.
+        i = 0;
         // printing the command
         printf("LAST Command: %s\n", lastCommand);
         // printing the error message or just OK if everything went ok.
@@ -35,31 +39,36 @@ int displayGame(void) {
             command[i++] = ptr;
             ptr = strtok(NULL, " ");
         }
-        strcpy(command, command[0]);
+        strcpy(commands, command[0]);
         strcpy(parameter, command[1]);
-//
-//        for (int i = 0; i < 2; ++i) {
-//            printf("%s\n", command[i]);
-//        }
+
+
+        for(int j = 0; commands[j]; j++){
+            commands[j] = toupper(commands[j]);
+        }
 
 
 
-        if (strcmp(command[0], "LD") == 0){
-            printf("LOADING CARDS FROM: %s\n\n", command[1]);
+        if (strcmp(commands, "LD") == 0){
+            printf("LOADING CARDS FROM: %s\n\n", parameter);
             strcpy(returnMsg, "OK");
         }
-        else if (strcmp(command[0], "SW") ==0){
-            printf("SHOWING CARDS\n\n");
-            strcpy(returnMsg, "OK");
+        else if (strcmp(commands, "SW") == 0){
+            if (strcmp(parameter,"") == 0){
+                printf("SHOWING CARDS\n\n");
+                strcpy(returnMsg, "OK");
+            } else {
+                strcpy(returnMsg, "Command (SW) does not have a parameter!");
+                command[1] = "";
+            }
+
         }
-        else if (strcmp(command[0], "SL") == 0){
-            printf("SPLITTING CARDS FROM: %s\n\n", command[1]);
+        else if (strcmp(commands, "SL") == 0){
+            printf("SPLITTING CARDS FROM: %s\n\n", parameter);
             strcpy(returnMsg, "OK");
         }
         else{
-            //printf("COMMAND NOT FOUND\n");
             strcpy(returnMsg,"COMMAND NOT FOUND");
-            //printf("\n");
         }
 
     }
